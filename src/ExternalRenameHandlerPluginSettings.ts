f8f50ccd2262a31994d9cf5ef30f9f49586356cf
Michael Naumov <mnaoumov@gmail.com>
2025-03-19 10:39:35 -0600

    Make polling configurable

--- src/ExternalRenameHandlerPluginSettings.ts
+++ src/ExternalRenameHandlerPluginSettings.ts
@@ -1,8 +1,8 @@
 import { PluginSettingsBase } from 'obsidian-dev-utils/obsidian/Plugin/PluginSettingsBase';
 
 export class ExternalRenameHandlerPluginSettings extends PluginSettingsBase {
+  public pollingIntervalInMilliseconds = 2000;
   public shouldUpdateLinks = true;
-  public shouldUsePolling = false;
 
   public constructor(data: unknown) {
     super();
--- src/ExternalRenameHandlerPluginSettingsTab.ts
+++ src/ExternalRenameHandlerPluginSettingsTab.ts
@@ -16,10 +16,21 @@ export class ExternalRenameHandlerPluginSettingsTab extends PluginSettingsTabBas
       });
 
     new SettingEx(this.containerEl)
-      .setName('Should use polling')
-      .setDesc('It is more reliable but slower')
-      .addToggle((toggle) => {
-        this.bind(toggle, 'shouldUsePolling');
+      .setName('Polling interval in milliseconds')
+      .setDesc(createFragment((f) => {
+        f.appendText('Polling is an additional mechanism to detect file changes.');
+        f.createEl('br');
+        f.appendText('The lower the value, the more CPU-intensive the plugin will be.');
+        f.createEl('br');
+        f.appendText('The higher the value, the more delay might occur to detect the missed file changes.');
+        f.createEl('br');
+        f.appendText('Use 0 to disable polling.');
+      }))
+      .addNumber((numberComponent) => {
+        this.bind(numberComponent, 'pollingIntervalInMilliseconds')
+          .setMin(0)
+          .setStep(100);
+        numberComponent.inputEl.required = true;
       });
   }
 
